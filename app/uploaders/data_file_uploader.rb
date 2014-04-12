3fd295bed842533b0011ea77dfa9977e9a748168
George F. Murphy <gmurphy@epublishing.com>
2014-04-12 13:35:44 -0500

    Process uploaded file in Uploader.

--- app/uploaders/data_file_uploader.rb
+++ app/uploaders/data_file_uploader.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
 
 class DataFileUploader < CarrierWave::Uploader::Base
+  include PurchaseFileImporter
+
   storage :file
+
+  process :import_purchases
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
